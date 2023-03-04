a66e877d9e5d07f4db182e3dfbf2781767527d88
Malthe Ørberg Pedersen <s194584@student.dtu.dk>
2023-03-04 16:45:11 +0100

    Refactored summary.py

--- scripts/summary.py
+++ scripts/summary.py
@@ -3,46 +3,56 @@ from matplotlib import pyplot as plt
 from importdata_csv import *
 from matplotlib.pyplot import (figure, subplot, plot, xlabel, ylabel,
                                xticks, yticks, legend, show, hist, title,
-                               subplots_adjust, scatter)
-
-attributes_to_be_analysed = ['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe']
-attributeName_to_index = {}
-for i, att in enumerate(attributeNames):
-    if att in attributes_to_be_analysed:
-        attributeName_to_index[att] = i
-# print(attributeName_to_index)
+                               subplots_adjust, scatter, savefig, suptitle)
 
 # Compute basic summary statistics
 summaries = {}
-for att in attributes_to_be_analysed:
-    mean = np.mean(X[:, attributeName_to_index[att]])
-    var = np.var(X[:, attributeName_to_index[att]])
-    sd = np.std(X[:, attributeName_to_index[att]])
+for i,att in enumerate(attributeNames):
+    col = X[:, i]
+    mean = np.mean(col)
+    var = np.var(col)
+    sd = np.std(col)
     summaries[att] = (mean, var, sd)
 
 # Histograms
 figure(figsize=(12, 10))
-for att in attributes_to_be_analysed:
-    subplot(3, 3, attributeName_to_index[att]+1)
-    hist(X[:, attributeName_to_index[att]], bins=30)
+for i,att in enumerate(attributeNames):
+    subplot(3, 3, i+1)
+    hist(X[:, i], bins=30)
     title(att)
     subplots_adjust(hspace=0.5, wspace=0.5)
+show()
 
 # Normalize data
 SX = X - np.ones((N, 1)) * X.mean(0)
 SX = SX * (1 / np.std(SX, 0))
 
+## Removes the potassium "outliers"
+# outlier_mask = (SX[:, 5] > 8)
+# valid_mask = np.logical_not(outlier_mask)
+#
+# # Finally we will remove these from the data set
+# X = X[valid_mask, :]
+# y = y[valid_mask]
+# N = len(y)
+#
+# SX = X - np.ones((N, 1)) * X.mean(0)
+# SX = SX * (1 / np.std(SX, 0))
+
+
 # Attribute scatter plot (box style)
-figure()
-for i, att in enumerate(attributes_to_be_analysed):
+figure(figsize=(9, 6))
+for i, att in enumerate(attributeNames):
     for c in range(C):
         class_mask = (y == c)
         noise = np.random.uniform(low=-0.25, high=0.25, size=len(SX[class_mask, 0]))
-        scatter(i+1+noise, SX[class_mask, attributeName_to_index[att]], marker='.', s=8, c=class_colors[c])
-xticks(range(1, len(attributes_to_be_analysed)+1), attributes_to_be_analysed)
-title('Attribute scatter plot')
+        scatter(i+1+noise, SX[class_mask, i], marker='.', s=8, c=class_colors[c],alpha=0.50)
+xticks(range(1, len(attributeNames)+1), attributeNames)
+title('Attribute scatter plot (Box style)')
 ylabel('Standardized attribute values')
 legend(classNames)
+# savefig("../plots/spbox-050alpha.svg",bbox_inches='tight')
+show()
 
 # Matrix scatter plot of attributes
 r_mask = np.ones(N, dtype=bool)
@@ -53,18 +63,21 @@ for m1 in range(M):
         subplot(M, M, m1*M + m2 + 1)
         for c in range(C):
             class_mask = ((y == c) & r_mask)
-            scatter(np.array(X[class_mask, m2]), np.array(X[class_mask, m1]), marker='.', s=8, c=class_colors[c])
+            scatter(np.array(X[class_mask, m2]), np.array(X[class_mask, m1]), marker='.', s=8, c=class_colors[c], alpha=0.50)
             if m1 == M-1:
+                xticks(fontsize=7)
                 xlabel(attributeNames[m2])
             else:
                 xticks([])
             if m2 == 0:
+                yticks(fontsize=7)
                 ylabel(attributeNames[m1])
             else:
                 yticks([])
             # ylim(0,X.max()*1.1)
             # xlim(0,X.max()*1.1)
-legend(classNames, bbox_to_anchor=(1.04, 0.5), loc="center left")
-
+subplot(M, M, 1).legend(classNames, bbox_to_anchor=(2, 2.3), loc='upper right')
+plt.suptitle('Attribute scatter plot matrix', fontsize=14)
 # Show all figures
-show()
+savefig("../plots/spm-050alpha.svg", bbox_inches='tight')
+# show()
