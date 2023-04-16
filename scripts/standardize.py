557cb25c758bbeb1a00a387c766e955ece218159
Malthe Ørberg Pedersen <s194584@student.dtu.dk>
2023-04-16 23:04:21 +0200

    Added standardization module

---
+++ scripts/standardize.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def prepend_zero(matrix):
+    return np.concatenate((np.ones((matrix.shape[0], 1)), matrix), 1)
+
+def standardize_X(known, test, intercept=False):
+    test = test - np.ones((test.shape[0], 1)) * known.mean(0)
+    # Check if any column has 0 std
+    std = np.std(known, 0)
+    b = np.argwhere(std == 0)
+    std[np.reshape(b, (len(b)))] = 1
+    complete = test * (1 / std)
+    if intercept:
+        return prepend_zero(complete)
+    return complete
+
+
+def standardize_train_test_pair(known, test, intercept=False):
+    return standardize_X(known,known,intercept=intercept), standardize_X(known,test,intercept=intercept)
+
+
+def standardize_y_for_training(target):
+    target = target - np.ones(target.shape[0]) * np.mean(target)
+    std = np.std(target)
+    if std == 0:
+        return 0
+    return target * (1 / std)
+
+
+def reverse_standardization_y(target, prediction):
+    mean = np.mean(target)
+    std = np.std(target)
+    return prediction * std + mean
